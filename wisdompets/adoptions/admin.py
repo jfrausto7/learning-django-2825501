7a51672e0606cbc58f1a2220dfd49bca28b6ea15
jfrausto7 <jacob_frausto@brown.edu>
2020-12-12 20:14:44 -0700

    Learned about and implemented models and fields in Django

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -2,3 +2,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pet
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species', 'breed', 'age', 'sex']
+
+
+
